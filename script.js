3024eecaf070edb1c9e7e09af599f8efff14c47a
Amit Chaudhary <chaudahry.amit@orangemantra.in>
2026-07-15 11:59:29 +0530

    add the changes in script

--- script.js
+++ script.js
@@ -489,7 +489,7 @@ class VehicleConditionOffcanvas {
   }
 
   open() {
-    this.subStep = "questions"; // remove this with question ---0>
+    this.subStep = "questions"; // remove this with questions ---0>
     this._renderSubStep();
     this.bsOffcanvas.show();
   }
